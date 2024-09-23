2134fc5aa650d67c48696a9a10307a16b8a6c16e
JustinCHughes <hcjustin713@gmail.com>
2024-09-23 16:54:27 -0500

    Completed exercise one chapter 1

---
+++ 1-Chapter/1-1HW.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	printf("\nAnswer on one line\n");
+	printf("she sells sea shells by the seashore\n");
+	printf("\nAnswer on three lines\n");
+	printf("she sells\nsea shells\nby the seashore\n");
+	printf("\nAnswer in a box\n");
+	printf("*****************************************\n");
+	printf("* She sells seas shells by the seashore *\n");
+	printf("*****************************************\n");
+	return 0;
+}
