2147cf63a452c6724daa442a974b8c94289f57a6
JustinCHughes <hcjustin713@gmail.com>
2024-09-23 17:13:27 -0500

    Completed Chapter 1 Exercise 3

---
+++ 1-Chapter/3-Problem/1-3HW.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	double miles = 26;
+	double yards = 385;
+
+	double kilometers;
+
+	kilometers = 1.609 * (miles + yards / 1760.0);
+
+	printf("\nA marathon is %f kilometers. \n\n", kilometers);
+	return 0;
+}
