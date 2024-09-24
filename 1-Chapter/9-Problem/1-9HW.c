98373ffdaff391e46b8cb5488cd6943fa2cc8fe6
JustinCHughes <hcjustin713@gmail.com>
2024-09-23 20:54:21 -0500

    Completed Chapter 1 Exercise 9

---
+++ 1-Chapter/9-Problem/1-9HW.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // Print the large letter C
+    printf("     CCCCCCCCCCCCCCCCCCCCC\n");
+    printf("    CCCCCCCCCCCCCCCCCCCCCCC\n");
+    printf("   CCC                   CCC\n");
+    printf("  CCC                     CCC\n");
+    printf(" CCC                       CCC\n");
+    printf("CCC                        CCC\n");
+    printf("CCC\n");
+    printf("CCC\n");
+    printf("CCC\n");
+    printf("CCC\n");
+    printf("CCC                         CCC\n");
+    printf("CCC                         CCC\n");
+    printf(" CCC                       CCC\n");
+    printf("  CCC                     CCC\n");
+    printf("   CCC                   CCC\n");
+    printf("    CCCCCCCCCCCCCCCCCCCCCCC\n");
+    printf("     CCCCCCCCCCCCCCCCCCCCC\n");
+
+    return 0;
+}
